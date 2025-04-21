91a692cb39316f5449c4bb2f08a920629448dbd2
Rajveer Saini <sainiveer210359@gmail.com>
2025-04-21 10:35:53 +0530

    Time: 0 ms (100%), Space: 42.3 MB (62.43%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+ 
+        if(!(str1+str2).equals(str2+str1))return "" ; 
+
+        int size1 = str1.length(); 
+        int size2 = str2.length(); 
+
+        String result = str1.substring(0 , gcdLength(size1 , size2)); 
+
+        return result ; 
+    }
+
+    public static int gcdLength(int size1 , int size2){
+        while(size2 != 0){
+            int temp = size2; 
+            size2 = size1 % size2 ; 
+            size1 = temp ; 
+        }
+        return size1 ; 
+    }
+}
