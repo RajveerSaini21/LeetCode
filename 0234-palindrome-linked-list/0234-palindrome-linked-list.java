f0d9f27b05c0c84527558215d6b9b12d29e9f02a
Rajveer Saini <sainiveer210359@gmail.com>
2026-02-05 12:01:04 +0530

    Time: 4 ms (77.49%), Space: 96.9 MB (30.07%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,6 +10,8 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+        
         ListNode slow = head ; 
         ListNode fast = head; 
         while(fast.next != null && fast.next.next != null){
