0aa726f2acaec429bc387be1c5a1439588a69411
Rajveer Saini <sainiveer210359@gmail.com>
2026-02-06 11:39:55 +0530

    Time: 35 ms (71.28%), Space: 98.6 MB (67.62%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -4,7 +4,7 @@ class Solution {
         long sum = 0, maxSum = 0;
         int left = 0;
         for (int right = 0; right < nums.length; right++) {
-            while (set.contains(nums[right])) {
+            while (set.contains(nums[right]) || set.size() == k) {
                 set.remove(nums[left]);
                 sum -= nums[left];
                 left++;
