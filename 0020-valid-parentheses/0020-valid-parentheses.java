179827aaa1a7a6268c2ce8ff2aa15f552d2e8459
Rajveer Saini <sainiveer210359@gmail.com>
2026-02-18 00:33:33 +0530

    Time: 3 ms (87.57%), Space: 43.5 MB (41.85%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,18 +4,15 @@ class Solution {
 
         for(char ch : s.toCharArray()){
             if(ch == '(' || ch == '{' || ch == '['){
-                stack.push(ch);
+                stack.push(ch) ; 
             }
             else{
-                if(stack.isEmpty()){
-                    return false; 
-                }
-                char top = stack.pop() ;
-                if ((ch == ')' && top != '(') || (ch == '}' && top != '{') || (ch == ']' && top != '[')) {
-                    return false;
-                } 
-            } 
+                if(stack.isEmpty()) return false ; 
+                char top = stack.pop() ; 
+                if(ch == '}' && top != '{' || ch == ')' && top != '(' || ch == ']' && top != '[') return false ; 
+            }
         }
-        return stack.isEmpty(); 
+
+        return stack.isEmpty() ; 
     }
 }
