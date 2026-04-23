38b4cf93ea9ddcbad49b0ea2e251d00815a9491f
Rajveer Saini <sainiveer210359@gmail.com>
2026-04-23 14:21:16 +0530

    Time: 1 ms (99.98%), Space: 77.4 MB (34.75%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,13 +1,31 @@
+//Brute Force approach
+// class Solution {
+//     public int maxSubArray(int[] arr) {
+//         int n = arr.length;  
+//         int maxSum = 0 ; 
+//         for(int i = 0 ; i < n; i++){
+//             int currSum = 0 ; 
+//             for(int j = i ; j < n ; j++){
+//                 currSum += arr[j] ; 
+//                 maxSum = Math.max(maxSum , currSum) ;
+//             }
+//         }
+//         return maxSum ; 
+//     }
+// }
+
+// Kadane's algorithm
 class Solution {
     public int maxSubArray(int[] arr) {
-        int currentSum = arr[0] ; 
-        int maxSum = arr[0];  
-
-        for(int i = 1 ; i < arr.length ; i++){
-            currentSum = Math.max(arr[i] , currentSum + arr[i]) ; 
-            maxSum = Math.max(maxSum , currentSum) ; 
+        int maxSum = Integer.MIN_VALUE ; 
+        int currSum = 0 ; 
+        for(int i = 0 ; i < arr.length ;i++){
+            currSum += arr[i] ; 
+            maxSum = Math.max(maxSum , currSum) ; 
+            if(currSum < 0){
+                currSum = 0 ; 
+            }
         }
-
         return maxSum ; 
     }
 }
